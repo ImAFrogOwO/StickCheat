30fe3067d66823d7860799a1affe10587b1a4ba2
ImAFrogOwO <dev.zero06@gmail.com>
2023-01-01 21:12:33 -0600

    Added Base

---
+++ StickCheat/StickCheat/Core.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+using UnityEngine;
+
+[assembly: MelonInfo(typeof(StickCheat.Core),"StickCheat","1.0.0","RSM && Granola")]
+
+namespace StickCheat
+{
+    public class Core : MelonMod
+    {
+    }
+}
